9c747bdf82bf5d3d3031da6e29281bce924a7dda
spiegelurs <kontakt@hohliebe.be>
2025-08-06 09:12:43 +0000

    Initial commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'urskurtmeier_magento2lieferschein',
+    __DIR__
+);
