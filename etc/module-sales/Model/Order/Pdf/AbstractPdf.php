ad4baf366e285214015d5d0f11c81a68ef75cdce
spiegelurs <kontakt@hohliebe.be>
2025-08-06 07:51:43 +0000

    Initial commit

---
+++ etc/module-sales/Model/Order/Pdf/AbstractPdf.php
@@ -0,0 +1,291 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sales\Model\Order\Pdf;
+
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\File\Pdf\Image;
+use Magento\MediaStorage\Helper\File\Storage\Database;
+use Magento\Sales\Model\RtlTextHandler;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Tax\Helper\Data as TaxHelper;
+
+/**
+ * Sales Order PDF abstract model
+ *
+ * phpcs:disable Magento2.Classes.AbstractApi
+ * @api
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @SuppressWarnings(PHPMD.TooManyFields)
+ * @since 100.0.2
+ */
+abstract class AbstractPdf extends Magento\Sales\Model\Order\Pdf\AbstractPdf
+{
+
+    /**
+     * Insert order to pdf page.
+     *
+     * @param \Zend_Pdf_Page $page
+     * @param \Magento\Sales\Model\Order $obj
+     * @param bool $putOrderId
+     * @return void
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    protected function insertOrder(&$page, $obj, $putOrderId = true)
+    {
+        if ($obj instanceof \Magento\Sales\Model\Order) {
+            $shipment = null;
+            $order = $obj;
+        } elseif ($obj instanceof \Magento\Sales\Model\Order\Shipment) {
+            $shipment = $obj;
+            $order = $shipment->getOrder();
+        }
+        $Address_x = 285;
+
+        $this->y = $this->y ? $this->y : 815;
+        $top = $this->y;
+
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0.45));
+        $page->setLineColor(new \Zend_Pdf_Color_GrayScale(0.45));
+        $page->drawRectangle(25, $top, 570, $top - 55);
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(1));
+        $this->setDocHeaderCoordinates([25, $top, 570, $top - 55]);
+        $this->_setFontRegular($page, 10);
+
+        if ($putOrderId) {
+            $page->drawText(__('Order # ') . $order->getRealOrderId(), 35, $top -= 30, 'UTF-8');
+            $top +=15;
+        }
+
+        $top -=30;
+        $page->drawText(
+            __('Order Date: ') .
+            $this->_localeDate->formatDate(
+                $this->_localeDate->scopeDate(
+                    $order->getStore(),
+                    $order->getCreatedAt(),
+                    true
+                ),
+                \IntlDateFormatter::MEDIUM,
+                false
+            ),
+            35,
+            $top,
+            'UTF-8'
+        );
+
+        $top -= 10;
+        $page->setFillColor(new \Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+        $page->setLineColor(new \Zend_Pdf_Color_GrayScale(0.5));
+        $page->setLineWidth(0.5);
+        $page->drawRectangle(25, $top, 275, $top - 25);
+        $page->drawRectangle(275, $top, 570, $top - 25);
+
+        /* Calculate blocks info */
+
+        /* Billing Address */
+        $billingAddress = $this->_formatAddress($this->addressRenderer->format($order->getBillingAddress(), 'pdf'));
+
+        /* Payment */
+        $paymentInfo = $this->_paymentData->getInfoBlock($order->getPayment())->setIsSecureMode(true)->toPdf();
+        $paymentInfo = $paymentInfo !== null ? htmlspecialchars_decode($paymentInfo, ENT_QUOTES) : '';
+        $payment = explode('{{pdf_row_separator}}', $paymentInfo);
+        foreach ($payment as $key => $value) {
+            if ($value && strip_tags(trim($value)) == '') {
+                unset($payment[$key]);
+            }
+        }
+        reset($payment);
+
+        /* Shipping Address and Method */
+        if (!$order->getIsVirtual()) {
+            /* Shipping Address */
+            $shippingAddress = $this->_formatAddress(
+                $this->addressRenderer->format($order->getShippingAddress(), 'pdf')
+            );
+            $shippingMethod = $order->getShippingDescription();
+        }
+
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontBold($page, 12);
+        $page->drawText(__('Sold to:'), 35, $top - 15, 'UTF-8');
+
+        if (!$order->getIsVirtual()) {
+            $page->drawText(__('Ship to:'), $Address_x, $top - 15, 'UTF-8');
+        } else {
+            $page->drawText(__('Payment Method:'), $Address_x, $top - 15, 'UTF-8');
+        }
+
+        $addressesHeight = $this->_calcAddressHeight($billingAddress);
+        if (isset($shippingAddress)) {
+            $addressesHeight = max($addressesHeight, $this->_calcAddressHeight($shippingAddress));
+        }
+
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(1));
+        $page->drawRectangle(25, $top - 25, 570, $top - 33 - $addressesHeight);
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontRegular($page, 10);
+        $this->y = $top - 40;
+        $addressesStartY = $this->y;
+
+        foreach ($billingAddress as $value) {
+            if ($value !== '') {
+                $text = [];
+                foreach ($this->string->split($value, 45, true, true) as $_value) {
+                    $text[] = $this->rtlTextHandler->reverseRtlText($_value);
+                }
+                foreach ($text as $part) {
+                    $page->drawText(strip_tags(ltrim($part ?: '')), 35, $this->y, 'UTF-8');
+                    $this->y -= 15;
+                }
+            }
+        }
+
+        $addressesEndY = $this->y;
+
+        if (!$order->getIsVirtual()) {
+            $this->y = $addressesStartY;
+            $shippingAddress = $shippingAddress ?? []; // @phpstan-ignore-line
+            foreach ($shippingAddress as $value) {
+                if ($value !== '') {
+                    $text = [];
+                    foreach ($this->string->split($value, 45, true, true) as $_value) {
+                        $text[] = $this->rtlTextHandler->reverseRtlText($_value);
+                    }
+                    foreach ($text as $part) {
+                        $page->drawText(strip_tags(ltrim($part ?: '')), $Address_x, $this->y, 'UTF-8');
+                        $this->y -= 15;
+                    }
+                }
+            }
+
+            $addressesEndY = min($addressesEndY, $this->y);
+            $this->y = $addressesEndY;
+
+            $page->setFillColor(new \Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+            $page->setLineWidth(0.5);
+            $page->drawRectangle(25, $this->y, 275, $this->y - 25);
+            $page->drawRectangle(275, $this->y, 570, $this->y - 25);
+
+            $this->y -= 15;
+            $this->_setFontBold($page, 12);
+            $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+            $page->drawText(__('Payment Method:'), 35, $this->y, 'UTF-8');
+            $page->drawText(__('Shipping Method:'), $Address_x, $this->y, 'UTF-8');
+
+            $this->y -= 10;
+            $page->setFillColor(new \Zend_Pdf_Color_GrayScale(1));
+
+            $this->_setFontRegular($page, 10);
+            $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+
+            $paymentLeft = 35;
+            $yPayments = $this->y - 15;
+        } else {
+            $yPayments = $addressesStartY;
+            $paymentLeft = $Address_x;
+        }
+
+        foreach ($payment as $value) {
+            if ($value && trim($value) != '') {
+                //Printing "Payment Method" lines
+                $value = preg_replace('/<br[^>]*>/i', "\n", $value);
+                foreach ($this->string->split($value, 45, true, true) as $_value) {
+                    $page->drawText(strip_tags(trim($_value ?: '')), $paymentLeft, $yPayments, 'UTF-8');
+                    $yPayments -= 15;
+                }
+            }
+        }
+
+        if ($order->getIsVirtual()) {
+            // replacement of Shipments-Payments rectangle block
+            $yPayments = min($addressesEndY, $yPayments);
+            $page->drawLine(25, $top - 25, 25, $yPayments);
+            $page->drawLine(570, $top - 25, 570, $yPayments);
+            $page->drawLine(25, $yPayments, 570, $yPayments);
+
+            $this->y = $yPayments - 15;
+        } else {
+            $topMargin = 15;
+            $methodStartY = $this->y;
+            $this->y -= 15;
+
+            if (isset($shippingMethod) && \is_string($shippingMethod)) {
+                foreach ($this->string->split($shippingMethod, 45, true, true) as $_value) {
+                    $page->drawText(strip_tags(trim($_value ?: '')), $Address_x, $this->y, 'UTF-8');
+                    $this->y -= 15;
+                }
+            }
+
+            $yShipments = $this->y;
+            $totalShippingChargesText = "(" . __('Total Shipping Charges') . " ";
+            if ($this->taxHelper->displayShippingPriceIncludingTax()) {
+                $totalShippingChargesText .= $order->formatPriceTxt($order->getShippingInclTax());
+            } else {
+                $totalShippingChargesText .= $order->formatPriceTxt($order->getShippingAmount());
+            }
+
+            if ($this->taxHelper->displayShippingBothPrices()
+                && $order->getShippingInclTax() != $order->getShippingAmount()) {
+                $totalShippingChargesText .= "(Incl. Tax " . $order->getShippingInclTax() . ")";
+            }
+            $totalShippingChargesText .= ")";
+
+            $page->drawText($totalShippingChargesText, $Address_x, $yShipments - $topMargin, 'UTF-8');
+            $yShipments -= $topMargin + 10;
+
+            $tracks = [];
+            if ($shipment) {
+                $tracks = $shipment->getAllTracks();
+            }
+            if (count($tracks)) {
+                $page->setFillColor(new \Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+                $page->setLineWidth(0.5);
+                $page->drawRectangle($Address_x, $yShipments, 510, $yShipments - 10);
+                $page->drawLine(400, $yShipments, 400, $yShipments - 10);
+                //$page->drawLine(510, $yShipments, 510, $yShipments - 10);
+
+                $this->_setFontRegular($page, 9);
+                $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+                //$page->drawText(__('Carrier'), 290, $yShipments - 7 , 'UTF-8');
+                $page->drawText(__('Title'), 290, $yShipments - 7, 'UTF-8');
+                $page->drawText(__('Number'), 410, $yShipments - 7, 'UTF-8');
+
+                $yShipments -= 20;
+                $this->_setFontRegular($page, 8);
+                foreach ($tracks as $track) {
+                    $maxTitleLen = 45;
+                    $trackTitle = $track->getTitle() ?? '';
+                    $endOfTitle = strlen($trackTitle) > $maxTitleLen ? '...' : '';
+                    $truncatedTitle = substr($trackTitle, 0, $maxTitleLen) . $endOfTitle;
+                    $page->drawText($truncatedTitle, 292, $yShipments, 'UTF-8');
+                    $page->drawText($track->getNumber(), 410, $yShipments, 'UTF-8');
+                    $yShipments -= $topMargin - 5;
+                }
+            } else {
+                $yShipments -= $topMargin - 5;
+            }
+
+            $currentY = min($yPayments, $yShipments);
+
+            // replacement of Shipments-Payments rectangle block
+            $page->drawLine(25, $methodStartY, 25, $currentY);
+            //left
+            $page->drawLine(25, $currentY, 570, $currentY);
+            //bottom
+            $page->drawLine(570, $currentY, 570, $methodStartY);
+            //right
+
+            $this->y = $currentY;
+            $this->y -= 15;
+        }
+    }
+
+}
